354d8f0341a9f36c5641ee98729f1da19d1f9cdf
Martin.S <songding@crnet.com.hk>
2019-02-18 14:52:47 +0800

    添加新的插件

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**  
+ * @Description 
+ * @author ${USER}
+ * @date ${DATE} ${TIME}
+ */
+public class ${NAME} {
+}
